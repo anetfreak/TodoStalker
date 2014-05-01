f762c343546e336dafd22b39a9230896e15fe387
anetfreak <anetfreak@gmail.com>
2014-05-01 13:29:30 -0700

    Adding code to compare the distance of stored todos and notify user..

--- src/com/stalker/LocationService.java
+++ src/com/stalker/LocationService.java
@@ -1,5 +1,8 @@
 package com.stalker;
 
+import java.util.ArrayList;
+import java.util.Map.Entry;
+
 import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -10,6 +13,8 @@ import android.location.Location;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.android.gms.location.LocationClient;
+import com.stalker.DBHelper.Todo;
+import com.stalker.places.PlacesList;
 
 public class LocationService extends IntentService {
 
@@ -42,22 +47,46 @@ public class LocationService extends IntentService {
 			HomeScreenActivity.currentLatitude = location.getLatitude();
 			HomeScreenActivity.currentLongitude = location.getLongitude();
 			
-			Intent intentNotify = new Intent(this, HomeScreenActivity.class);
-			PendingIntent notifyPendingIntent = PendingIntent.getActivity(this, 1, intentNotify, 0);
+			//Check for distance and accordingly notify the user here...
+			//Fetch all location from databases and compare here
+			Location todoLoc = new Location("Current Location");
+			todoLoc.setLatitude(location.getLatitude());
+			todoLoc.setLongitude(location.getLongitude());
 			
-			String notificationText = "One or more of your todo's can be completed from a nearby location.";
-			myNotification = new NotificationCompat.Builder(getApplicationContext())
-			.setContentTitle("TodoStalker")
-			.setContentText(notificationText)
-			.setTicker("TodoStalker")
-			.setWhen(System.currentTimeMillis())
-			.setDefaults(Notification.DEFAULT_SOUND)
-			.setAutoCancel(true)
-			.setSmallIcon(R.drawable.stalker)
-			.setContentIntent(notifyPendingIntent)
-			.build();
+			ArrayList<Todo> nearbyTodos = new ArrayList<Todo>();
+			for(Entry<Todo, PlacesList> todoMap : HomeScreenActivity.TODOtoPlaces.entrySet()) {
+				for(int i = 0; i < todoMap.getValue().results.size(); i++) {
+					
+					Location placeLoc = new Location("Place Location");
+					todoLoc.setLatitude(todoMap.getValue().results.get(i).geometry.location.lat);
+					todoLoc.setLongitude(todoMap.getValue().results.get(i).geometry.location.lng);
+					float distance = todoLoc.distanceTo(placeLoc);
+					if(distance <= 500) {
+						nearbyTodos.add(todoMap.getKey());
+					}
+				}
+			}
+			//Check if there are any nearby todos that the user can go and resolve.
+			if( nearbyTodos.size() > 0 ) {
+				//Send a notification to the user.
+				
+				Intent intentNotify = new Intent(this, HomeScreenActivity.class);
+				PendingIntent notifyPendingIntent = PendingIntent.getActivity(this, 1, intentNotify, 0);
+				
+				String notificationText = nearbyTodos.size() + " of your todo tasks are in your vicinity..";
+				myNotification = new NotificationCompat.Builder(getApplicationContext())
+				.setContentTitle("TodoStalker")
+				.setContentText(notificationText)
+				.setTicker("TodoStalker")
+				.setWhen(System.currentTimeMillis())
+				.setDefaults(Notification.DEFAULT_SOUND)
+				.setAutoCancel(true)
+				.setSmallIcon(R.drawable.stalker)
+				.setContentIntent(notifyPendingIntent)
+				.build();
 
-			notificationManager.notify(NOTIFICATION_ID, myNotification);
+				notificationManager.notify(NOTIFICATION_ID, myNotification);
+			}
 			
 		}
 	}
